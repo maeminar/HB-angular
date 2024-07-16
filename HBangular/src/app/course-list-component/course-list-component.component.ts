099eabebebad0ff26cdb72e31b0118da4f10ff2f
Emma <emmaminary@gmail.com>
2024-07-16 12:18:28 +0200

    initialze

---
+++ HBangular/src/app/course-list-component/course-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-list-component.component.html',
+  styleUrl: './course-list-component.component.css'
+})
+export class CourseListComponentComponent {
+
+}
---
+++ HBangular/src/app/registration-form-component/registration-form-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-form-component.component.html',
+  styleUrl: './registration-form-component.component.css'
+})
+export class RegistrationFormComponentComponent {
+
+}
